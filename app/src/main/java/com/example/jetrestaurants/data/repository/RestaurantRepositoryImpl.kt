007b962565fdf642700d7b042501c7a7daf7a49b
Alex Madaras <62357167+MadarasAlex@users.noreply.github.com>
2025-04-04 18:49:08 +0200

    Implemented the API connection using Retrofit for the client side.

---
+++ app/src/main/java/com/example/jetrestaurants/data/repository/RestaurantRepositoryImpl.kt
@@ -0,0 +1,4 @@
+package com.example.jetrestaurants.data.repository
+
+class RestaurantRepositoryImpl {
+}
---
+++ app/src/main/java/com/example/jetrestaurants/ui/viewmodel/RestaurantsViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.jetrestaurants.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class RestaurantsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
